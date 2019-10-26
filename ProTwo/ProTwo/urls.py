dbd19b8f1d211956ba64cad969cef98c9630c03f
Tarun-msrit <tarun.ragothaman@gmail.com>
2019-10-26 21:50:04 +0530

    first commit

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url
 from appTwo import views
 
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$',views.index,name='index'),
 ]
