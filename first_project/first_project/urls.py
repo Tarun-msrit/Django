f2ec42cfaac68e1f6557854faa87f5546510b13c
Tarun-msrit <tarun.ragothaman@gmail.com>
2020-05-31 23:55:00 +0530

    first commit

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^first_app/',include('first_app.urls')),
+    url(r'^newapp/',include('newapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
